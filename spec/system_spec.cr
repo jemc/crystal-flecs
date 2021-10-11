ad80c13e93dc5f6910df6e70c55d7bd189aab44e
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2021-10-11 14:55:08 -0700

    Separate system specs from more general world specs.

---
+++ spec/system_spec.cr
@@ -0,0 +1,78 @@
+require "./spec_helper"
+
+module SystemExamples
+  component Age do
+    property years : UInt32 = 0
+    property other_years : UInt32 = 0
+    def initialize(@years)
+    end
+  end
+
+  system IncrementAge do
+    phase "EcsOnUpdate"
+
+    term age : Age, write: true
+
+    def run(iter)
+      iter.each { |row|
+        row.update_age { |age|
+          age.years += 1
+          age.other_years += 1
+          age
+        }
+      }
+    end
+  end
+
+  system PrintAge do
+    phase "EcsOnStore"
+
+    term age : Age
+
+    def run(iter)
+      @total_age = 0
+      iter.each { |row| @total_age += row.age.years }
+      @mean_age = @total_age / iter.count
+    end
+
+    property total_age : Int32 = 0
+    property mean_age : Float64 = 0
+  end
+end
+
+describe System do
+  it "can be declared and ran" do
+    world = World.init
+
+    SystemExamples::Age.register(world)
+    alice = world.entity_init(name: "Alice")
+    bob = world.entity_init(name: "Bob")
+
+    world.set(alice, SystemExamples::Age.new(99_u32))
+    world.set(bob, SystemExamples::Age.new(88_u32))
+
+    SystemExamples::IncrementAge::QUERY_STRING
+      .should eq "[inout] SystemExamples__Age"
+    SystemExamples::PrintAge::QUERY_STRING
+      .should eq "[in] SystemExamples__Age"
+
+    SystemExamples::IncrementAge.new.register(world)
+    system = SystemExamples::PrintAge.new
+    system.register(world)
+
+    system.total_age.should eq 0
+    system.mean_age.should eq 0
+
+    world.progress
+
+    system.total_age.should eq 189
+    system.mean_age.should eq 94.5
+
+    world.progress
+
+    system.total_age.should eq 191
+    system.mean_age.should eq 95.5
+
+    world.fini
+  end
+end
--- spec/world_spec.cr
+++ spec/world_spec.cr
@@ -1,40 +1,10 @@
 require "./spec_helper"
 
-module Example
+module WorldSpec
   component Age do
     property years : UInt64 = 0
     def initialize(@years)
     end
-  end
-
-  system IncrementAge do
-    phase "EcsOnUpdate"
-
-    term age : Age, write: true
-
-    def run(iter)
-      iter.each { |row|
-        row.update_age { |age|
-          age.years += 1
-          age
-        }
-      }
-    end
-  end
-
-  system PrintAge do
-    phase "EcsOnStore"
-
-    term age : Age
-
-    def run(iter)
-      @total_age = 0
-      iter.each { |row| @total_age += row.age.years }
-      @mean_age = @total_age / iter.count
-    end
-
-    property total_age : Int32 = 0
-    property mean_age : Float64 = 0
   end
 end
 
@@ -51,45 +21,12 @@ describe World do
 
   it "can set and get the value of a component on an entity" do
     world = World.init
-    Example::Age.register(world)
+    WorldSpec::Age.register(world)
 
     alice = world.entity_init(name: "Alice")
 
-    world.set(alice, Example::Age.new(99_u64))
-    world.get(alice, Example::Age).years.should eq 99_u64
-
-    world.fini
-  end
-
-  it "can run a system" do
-    world = World.init
-
-    Example::Age.register(world)
-    alice = world.entity_init(name: "Alice")
-    bob = world.entity_init(name: "Bob")
-
-    world.set(alice, Example::Age.new(99_u64))
-    world.set(bob, Example::Age.new(88_u64))
-
-    Example::IncrementAge::QUERY_STRING.should eq "[inout] Example__Age"
-    Example::PrintAge::QUERY_STRING.should eq "[in] Example__Age"
-
-    Example::IncrementAge.new.register(world)
-    system = Example::PrintAge.new
-    system.register(world)
-
-    system.total_age.should eq 0
-    system.mean_age.should eq 0
-
-    world.progress
-
-    system.total_age.should eq 189
-    system.mean_age.should eq 94.5
-
-    world.progress
-
-    system.total_age.should eq 191
-    system.mean_age.should eq 95.5
+    world.set(alice, WorldSpec::Age.new(99_u64))
+    world.get(alice, WorldSpec::Age).years.should eq 99_u64
 
     world.fini
   end
