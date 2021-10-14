a2b518960a56d1ca13f2e56a570344b1c3635869
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2021-10-13 20:48:31 -0700

    Allow components to have before_register and after_register hooks.

---
+++ spec/component_spec.cr
@@ -0,0 +1,29 @@
+require "./spec_helper"
+
+module ComponentExamples
+  component Hooks do
+    property dummy : Int32 = 0
+
+    HOOKS_RAN = [] of Symbol
+    ID_WHEN_HOOKS_RAN = [] of UInt64
+
+    def self.before_register(world : World)
+      HOOKS_RAN << :before
+      ID_WHEN_HOOKS_RAN << id(world)
+    end
+    def self.after_register(world : World)
+      HOOKS_RAN << :after
+      ID_WHEN_HOOKS_RAN << id(world)
+    end
+  end
+end
+
+describe Component do
+  it "can run custom hooks before and after registering with the world" do
+    ComponentExamples::Hooks.register(world)
+
+    ComponentExamples::Hooks::HOOKS_RAN.should eq [:before, :after]
+    ComponentExamples::Hooks::ID_WHEN_HOOKS_RAN.first.should eq 0
+    ComponentExamples::Hooks::ID_WHEN_HOOKS_RAN.last.should_not eq 0
+  end
+end
--- src/flecs/component.cr
+++ src/flecs/component.cr
@@ -19,6 +19,12 @@ module ECS::Component::StaticMethods
 
   # Register this component within the given World.
   def register(world : ECS::World)
+    the_self = self
+    # If the component author declared a before_register method, run it now.
+    if the_self.responds_to?(:before_register)
+      the_self.before_register(world)
+    end
+
     desc = ECS::LibECS::ComponentDesc.new
     desc.entity.name = ecs_name
     desc.size = sizeof(self)
@@ -30,6 +36,12 @@ module ECS::Component::StaticMethods
       if id == 0_u64
 
     save_id(world, id)
+
+    # If the component author declared an after_register method, run it now.
+    if the_self.responds_to?(:after_register)
+      the_self.after_register(world)
+    end
+
     id
   end
 end
