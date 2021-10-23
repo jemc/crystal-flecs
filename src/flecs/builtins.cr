f7edac31067847e3ad2dee678e2aade39d1562e5
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2021-10-23 09:26:47 -0700

    More builtin events, and better visual organization of builtins.

--- src/flecs/builtins.cr
+++ src/flecs/builtins.cr
@@ -2,8 +2,8 @@ require "./entity"
 require "./component"
 
 module ECS::Builtins
-  ECS.extern_entity(OnAdd) { ECS_NAME = "flecs.core.OnAdd" }
-  ECS.extern_entity(OnRemove) { ECS_NAME = "flecs.core.OnRemove" }
+  ##
+  # Core Components
 
   ECS.extern_component Component do
     ECS_NAME = "Component"
@@ -12,6 +12,18 @@ module ECS::Builtins
     def initialize(@size, @alignment)
     end
   end
+
+  ##
+  # Core Events
+
+  ECS.extern_entity(OnAdd)    { ECS_NAME = "flecs.core.OnAdd" }
+  ECS.extern_entity(OnRemove) { ECS_NAME = "flecs.core.OnRemove" }
+  ECS.extern_entity(OnSet)    { ECS_NAME = "flecs.core.OnSet" }
+  ECS.extern_entity(UnSet)    { ECS_NAME = "flecs.core.UnSet" }
+  ECS.extern_entity(OnDelete) { ECS_NAME = "flecs.core.OnDelete" }
+
+  ##
+  # Meta Components
 
   ECS.extern_component Member do
     ECS_NAME = "flecs.meta.Member"
